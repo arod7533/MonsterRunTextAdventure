51596b33f4f597a41afebfc4519a110984499436
Tony Rodriguez <arod7533@gmail.com>
2019-09-02 17:02:34 -0400

    Add project files.

---
+++ MonsterRunTextAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonsterRunTextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
